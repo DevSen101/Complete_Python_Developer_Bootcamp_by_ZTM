13fb39a79f1abc26e66cc46a7a54591c6fee90b7
Dev Sen <devksen.personal@gmail.com>
2026-01-15 20:47:22 +0530

    add: generator exercise

---
+++ 7. Generators/Exercise.py
@@ -0,0 +1,34 @@
+# WAP to print fibonacci series using generators
+
+
+# def fib(num):
+#     a = 0
+#     b = 1
+#     for i in range(num):
+#         yield a
+#         temp = a
+#         a = b
+#         b = temp + b
+
+
+# for x in fib(10):
+#     print(x)
+
+
+# for list------
+
+
+def fib2(num):
+    a = 0
+    b = 1
+    result = []
+
+    for i in range(num):
+        result.append(a)
+        temp = a
+        a = b
+        b = temp + b
+    return result
+
+
+print(fib2(10))
