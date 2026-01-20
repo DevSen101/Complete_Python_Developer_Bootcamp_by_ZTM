4faab8e86e549240667c50e040c499a5ef4e7bca
Dev Sen <devksen.personal@gmail.com>
2026-01-20 21:11:50 +0530

    learn: start web scraping in python

---
+++ 14. Web Scraping with Python/1. Intro.py
@@ -0,0 +1,4 @@
+# (1). Web Scraping - stands for collecting data or grabbing data from different websites...
+# (2). we can easily go to any websites url + '/robots.txt' endpoint to see the ethical data to scrap which website allows us. while there are some tools which provide us to do it unehically like chrome web scraper.
+# (3). Some websites provides us free APIs to access data like jsonplaceholder, starverse API, udemy API etc.
+# (4). Googlebot works like when we click on api endpoint so its check all the information and then search then gives us the page we look for its also called SEO. evry search engine has their bot.
