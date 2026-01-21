ee2918af5f7e36ea7a061a41fd5d14681fb612ea
Dev Sen <devksen.personal@gmail.com>
2026-01-21 17:57:27 +0530

    learn: serve files using templates and static using render templates

--- 15. Web Development with Python/web Server/server.py
+++ 15. Web Development with Python/web Server/server.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "Server running..by dev."
+    return render_template('./index.html')
+
+
+@app.route("/home")
+def home_route():
+    return render_template("./index.html")
 
 
 if __name__ == "__main__":
