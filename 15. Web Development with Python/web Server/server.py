32bb7c2ada6b394465af31f83f6d34fb5d10ba0c
Dev Sen <devksen.personal@gmail.com>
2026-01-22 12:54:07 +0530

    learn: wrap up with web dev

--- 15. Web Development with Python/web Server/server.py
+++ 15. Web Development with Python/web Server/server.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return render_template('./index.html')
+# @app.route("/")
+# def hello_world():
+#     return render_template('./index.html')
+
+
+@app.route("/<username>")
+def hello_world2(username=None):
+    return render_template("./index.html", name = username)
 
 
 @app.route("/home")
